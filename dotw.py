b399afb878cac673babc720fb0d427c150fef8b4
gonzoreader <gonzoreader@gmail.com>
2024-01-02 22:15:56 +0000

    finalizing input and print functions

---
+++ dotw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    # defining name user input "nausin" 
+    nausin = input("Whats your name? :")
+    
+    # defining date user input "dausin"
+    dausin = input("What day of the week is it? :")
+
+    #print("Hello,", nausin + "!", "Happy", dausin + "!")
+    print(f'Hello, {nausin}!  Happy {dausin}!')
+main()
